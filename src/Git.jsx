0763ee6d86512c3ffa7df963408d25fed44b4a56
Buhari <sybuhari234@gmail.com>
2025-02-05 15:58:58 +0530

    updated

---
+++ src/Git.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Git = () => {
+  return (
+    <div>
+      GIt
+    </div>
+  )
+}
+
+export default Git
---
+++ src/Git2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Git2 = () => {
+  return (
+    <div>
+      Git 2
+    </div>
+  )
+}
+
+export default Git2
